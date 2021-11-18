a70d1d8bc986ed86104c6fc9c0e335409dfab274
Jędrzej Garbacz <78218773+f0ksiak@users.noreply.github.com>
2021-11-18 14:01:27 +0100

    v0.1

---
+++ script.js
@@ -0,0 +1,28 @@
+function radioSelector() {
+    var x = document.getElementById("radio");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function radioSelect1() {
+    var x = document.getElementById("player1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function radioSelect2() {
+    var x = document.getElementById("player2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  radioSelector()
+  radioSelect1()
+  radioSelect2()
