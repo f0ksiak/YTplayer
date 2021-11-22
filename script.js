04f883a3ca2e2ed252c8cc8c6c3580114647788a
Jędrzej Garbacz <f00ksuu@gmail.com>
2021-11-22 14:27:28 +0100

    v0.2

--- script.js
+++ script.js
@@ -1,28 +1,63 @@
-function radioSelector() {
-    var x = document.getElementById("radio");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
+
+  function lofi() {
+    document.getElementById("video").src= "https://www.youtube.com/embed/5qap5aO4i9A?controls=0&enablejsapi=1";
+
   }
-  function radioSelect1() {
-    var x = document.getElementById("player1");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
+
+  function house() {
+    document.getElementById("video").src= "https://www.youtube.com/embed/GDQnA1LVCWA?controls=0&enablejsapi=1" ;
+    
   }
-  function radioSelect2() {
-    var x = document.getElementById("player2");
-    if (x.style.display === "none") {
-      x.style.display = "block";
+  function rock(){
+    document.getElementById("video").src= "https://www.youtube.com/embed/-v9Pm9PYU8E?controls=0&enablejsapi=1";
+  }
+
+  function hidePlayer(){
+    var x = document.getElementById("video");
+    x.style.display = "none";
+  }
+  function hideRadio(){
+    var y = document.getElementById("buttons");
+    if (y.style.display === "none") {
+      y.style.display = "block";
     } else {
-      x.style.display = "none";
+      y.style.display = "none";
     }
   }
 
-  radioSelector()
-  radioSelect1()
-  radioSelect2()
+
+  hidePlayer()
+  hideRadio()
+
+var player;
+
+
+function onYouTubePlayerAPIReady() {
+
+    player = new YT.Player('video', {
+        events: {
+
+            'onReady': onPlayerReady
+        }
+    });
+}
+
+function onPlayerReady(event) {
+
+
+    var playButton = document.getElementById("play-button");
+    playButton.addEventListener("click", function() {
+        player.playVideo();
+    });
+
+    var pauseButton = document.getElementById("pause-button");
+    pauseButton.addEventListener("click", function() {
+        player.pauseVideo();
+    });
+  }
+
+
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
