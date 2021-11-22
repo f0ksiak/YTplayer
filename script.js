ea32d91a2313ef5f07eb8ecd4e5acd652b1fa894
Jędrzej Garbacz <f00ksuu@gmail.com>
2021-11-22 15:44:32 +0100

    v0.4

--- script.js
+++ script.js
@@ -12,22 +12,6 @@
     document.getElementById("video").src= "https://www.youtube.com/embed/-v9Pm9PYU8E?controls=0&enablejsapi=1";
   }
 
-  function hidePlayer(){
-    var x = document.getElementById("video");
-    x.style.display = "none";
-  }
-  function hideRadio(){
-    var y = document.getElementById("buttons");
-    if (y.style.display === "none") {
-      y.style.display = "block";
-    } else {
-      y.style.display = "none";
-    }
-  }
-
-
-  hidePlayer()
-  hideRadio()
 
 var player;
 
@@ -61,3 +45,16 @@ var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/player_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+function dropdownMenu() {  
+  var list = document.getElementById("option");  
+  document.getElementById("station").value = list.options[list.selectedIndex].text;  
+  var strUser = list.value;
+  if(strUser=="Lofi"){
+    document.getElementById("video").src= "https://www.youtube.com/embed/5qap5aO4i9A?controls=0&enablejsapi=1";
+  } if(strUser=="House"){
+    document.getElementById("video").src= "https://www.youtube.com/embed/GDQnA1LVCWA?controls=0&enablejsapi=1";
+  } if(strUser=="Rock"){
+    document.getElementById("video").src= "https://www.youtube.com/embed/-v9Pm9PYU8E?controls=0&enablejsapi=1";
+  }
+  }  
