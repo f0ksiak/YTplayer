6bb968455b07332faaf50840a378e959b2476b82
Jędrzej Garbacz <f00ksuu@gmail.com>
2021-11-23 14:59:24 +0100

    v0.7

--- script.js
+++ script.js
@@ -24,6 +24,25 @@ function onPlayerReady(event) {
     });
   }
 
+  function exit() {
+    var radio = document.getElementById("radio")
+    var playButton = document.getElementById("yt-button");
+          radio.style.display="none";
+          player.stopVideo();
+          playButton.innerHTML = '<i class="fas fa-play"></i>';
+
+  }
+  function minimalize() {
+    var radio = document.getElementById("radio")
+          radio.style.display="none";
+
+  }
+  function showRadio(){
+    var radio = document.getElementById("radio")
+          radio.style.display="block";
+  }
+  
+
 
 var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/player_api";
@@ -34,16 +53,17 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 function dropdownMenu() {  
   var playButton = document.getElementById("yt-button");
   var list = document.getElementById("option");  
+  var video = document.getElementById("video");
   document.getElementById("station").value = list.options[list.selectedIndex].text;  
   var strUser = list.value;
   if(strUser=="Lofi"){
-    document.getElementById("video").src= "https://www.youtube.com/embed/5qap5aO4i9A?controls=0&enablejsapi=1";
+    video.src= "https://www.youtube.com/embed/5qap5aO4i9A?controls=0&enablejsapi=1";
     playButton.innerHTML = '<i class="fas fa-play"></i>';
   } if(strUser=="House"){
-    document.getElementById("video").src= "https://www.youtube.com/embed/GDQnA1LVCWA?controls=0&enablejsapi=1";
+    video.src= "https://www.youtube.com/embed/GDQnA1LVCWA?controls=0&enablejsapi=1";
     playButton.innerHTML = '<i class="fas fa-play"></i>';
   } if(strUser=="Rock"){
-    document.getElementById("video").src= "https://www.youtube.com/embed/-v9Pm9PYU8E?controls=0&enablejsapi=1";
+    video.src= "https://www.youtube.com/embed/-v9Pm9PYU8E?controls=0&enablejsapi=1";
     playButton.innerHTML = '<i class="fas fa-play"></i>';
   }
   }  
